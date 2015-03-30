d89bdf3a450ea1f41ffd6829ba080b28d39eeb1c
Chintan Shah <cgintan1998@gmail.com>
2015-03-30 10:21:56 -0600

    Other errors fixing
    Signed-off-by: Chintan Shah <cgintan1998@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/LocationTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/LocationTile.java
@@ -121,17 +121,17 @@ public class LocationTile extends QSTile<QSTile.BooleanState> {
             case Settings.Secure.LOCATION_MODE_BATTERY_SAVING:
                 state.contentDescription = mContext.getString(
                         R.string.accessibility_quick_settings_location_battery_saving);
-                state.iconId = ResourceIcon.get(R.drawable.ic_qs_location_battery_saving);
+                state.icon = ResourceIcon.get(R.drawable.ic_qs_location_battery_saving);
                 break;
             case Settings.Secure.LOCATION_MODE_SENSORS_ONLY:
                 state.contentDescription = mContext.getString(
                         R.string.accessibility_quick_settings_location_gps_only);
-                state.iconId = mEnable;
+                state.icon = mEnable;
                 break;
             case Settings.Secure.LOCATION_MODE_HIGH_ACCURACY:
                 state.contentDescription = mContext.getString(
                         R.string.accessibility_quick_settings_location_high_accuracy);
-                state.iconId = mEnable;
+                state.icon = mEnable;
                 break;
             default:
                 state.contentDescription = mContext.getString(
--- packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
+++ packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
@@ -566,8 +566,6 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
 
         // Dismiss Recents to the focused Task or Home
         dismissRecentsToFocusedTaskOrHome(true);
-
-        mRecentsView.endFABanimation();
     }
 
     /** Called when debug mode is triggered */
@@ -603,7 +601,6 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
     public void onExitToHomeAnimationTriggered() {
         // Animate the SystemUI scrim views out
         mScrimViews.startExitRecentsAnimation();
-        mRecentsView.endFABanimation();
     }
 
     @Override
@@ -614,13 +611,11 @@ public class RecentsActivity extends Activity implements RecentsView.RecentsView
     public void onTaskLaunchFailed() {
         // Return to Home
         dismissRecentsToHomeRaw(true);
-        mRecentsView.endFABanimation();
     }
 
     @Override
     public void onAllTaskViewsDismissed() {
         mFinishLaunchHomeRunnable.run();
-        mRecentsView.endFABanimation();
     }
 
     @Override
